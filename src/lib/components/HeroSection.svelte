8b4ee5e4b6ad82ea4a0c12d97311e35d9c6e3bf8
Shamim Hossain <hossains159@gmail.com>
2023-02-26 23:10:18 +0600

    refactor: move hero section to a separate component

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,36 @@
+<section
+	id="section-hero"
+	class="space-y-4 text-lg leading-tight"
+>
+	<h1 class="text-2xl font-bold">
+		Meet with App Store experts
+	</h1>
+	<h2 class="mt-2 font-semibold text-gray-400">
+		February 28 to April 13, 2023
+	</h2>
+	<p>
+		Want to make your product pages even better? Have
+		questions about in-app events or implementing
+		subscriptions? Connect with App Store experts and learn
+		how you can make the most of these features. Discover
+		how to attract new customers, test marketing strategies,
+		optimize subscriptions, and so much more. Live
+		presentations with Q&A will be held in multiple time
+		zones and languages.
+	</p>
+	<p>
+		Registration is open to members of the Apple Developer
+		Program. Please sign in with your Apple ID associated
+		with your membership in the Apple Developer Program.
+	</p>
+	<div class="flex">
+		<a
+			href="https://developer.apple.com/events/view/"
+			target="_blank"
+			rel="noreferrer"
+			class="flex items-center justify-center rounded-full bg-gradient-to-t from-blue-600 to-blue-500 px-3 py-1 text-sm transition hover:from-blue-500 hover:to-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-600"
+		>
+			View schedule and register
+		</a>
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import AppleStoreLogo from '$lib/assets/app-store-sessions.svg';
 	import EventCard from '$lib/components/EventCard.svelte';
+	import HeroSection from '$lib/components/HeroSection.svelte';
 
+	// TODO: take this list from API
 	const eventsList = [
 		{
 			id: 1,
@@ -43,42 +45,7 @@
 		height="100"
 	/>
 
-	<section
-		id="section-hero"
-		class="space-y-4 text-lg leading-tight"
-	>
-		<h1 class="text-2xl font-bold">
-			Meet with App Store experts
-		</h1>
-		<h2 class="mt-2 font-semibold text-gray-400">
-			February 28 to April 13, 2023
-		</h2>
-		<p>
-			Want to make your product pages even better? Have
-			questions about in-app events or implementing
-			subscriptions? Connect with App Store experts and
-			learn how you can make the most of these features.
-			Discover how to attract new customers, test marketing
-			strategies, optimize subscriptions, and so much more.
-			Live presentations with Q&A will be held in multiple
-			time zones and languages.
-		</p>
-		<p>
-			Registration is open to members of the Apple Developer
-			Program. Please sign in with your Apple ID associated
-			with your membership in the Apple Developer Program.
-		</p>
-		<div class="flex">
-			<a
-				href="https://developer.apple.com/events/view/"
-				target="_blank"
-				rel="noreferrer"
-				class="flex items-center justify-center rounded-full bg-gradient-to-t from-blue-600 to-blue-500 px-3 py-1 text-sm transition hover:from-blue-500 hover:to-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-600"
-			>
-				View schedule and register
-			</a>
-		</div>
-	</section>
+	<HeroSection />
 
 	<section id="section-event-list" class="mt-10">
 		<ul class="flex flex-col gap-4">
